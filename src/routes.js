c423c08fa90853ba29d2dddfd060849fe8d5784a
Rakhan Dawood <rakhandawood@Rakhans-MacBook-Pro.local>
2025-03-10 21:21:09 +0530

    Complete code reorganization

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,20 @@ const blogController = require('./controllers/blogController');
 const combinedController = require('./controllers/combinedController');
 
 function setupRoutes(app) {
+  // Root route
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'YouTube Transcript to Blog API',
+      endpoints: {
+        health: '/api/health',
+        transcript: '/api/transcript',
+        blog: '/api/blog',
+        process: '/api/process'
+      },
+      docs: 'Make POST requests to the endpoints with appropriate JSON data'
+    });
+  });
+
   // Individual endpoints
   app.post('/api/transcript', transcriptController.getTranscript);
   app.post('/api/blog', blogController.generateBlog);
